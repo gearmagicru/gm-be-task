0eb45fe6a5759e5b048bbecb0747e8c6feb5134f
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-02-11 15:11:30 +0300

    Файлы локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Этот файл является частью модуля веб-приложения GearMagic.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'System tasks',
+    '{description}' => 'Performs tasks on the client side specified by the server',
+    '{permissions}' => []
+];
